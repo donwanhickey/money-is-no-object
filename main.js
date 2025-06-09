760209dd4ea0e9695595c33a656d45e64506f7b8
unknown <donwanhickey@gmail.com>
2025-06-09 02:08:48 -0400

    final commit

--- main.js
+++ main.js
@@ -12,6 +12,60 @@ if (userObj.userRole === "ADMIN") {
 };
 
 
+const getEmail = function(userObj) {
+    return userObj.firstName[0].toLowerCase() + 
+           userObj.lastName.toLowerCase() +
+          '.prsvr@gmail.com';
+    
+   
+};
+
+const getPlaylistLength = function(getPlaylistObj) {
+    const numberOfSongs = getPlaylistObj.songs.length;
+    return numberOfSongs;
+};
+
+const getHardestHomework = function(homeworks) {
+    if (homeworks.length === 0) {
+        return '';
+
+    }
+     let lowestScore = homeworks[0].averageScore;
+     let hardest = homeworks[0].name;
+      
+     for (let i = 1; i < homeworks.length; i++) {
+        if (homeworks[i].averageScore < lowestScore) {
+            lowestScore = homeworks[i].averageScore;
+            hardest = homeworks[i].name;
+        }
+        }
+        return hardest
+     };
+
+const createPhonebook = function(names, numbers) {
+    const phonebook = {};
+
+    for (let i = 0; i < names.length; i++) 
+     phonebook[names[i]]  =  numbers[i];
+
+           return phonebook;
+
+   };
+
+
+   
+    
+  
+
+
+
+
+ 
+
+
+
+
+
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
 // └─────────────────────────────────────┘
